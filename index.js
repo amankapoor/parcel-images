18e68763ccb7d5b714efad85e9731fbafd1d9ce7
amankapoor <aman.sk95@gmail.com>
2018-08-02 01:23:09 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Main from "./src/main";
+const App = () => {
+  return (
+    <div>
+      <Main />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ src/main.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Camp from "../assets/camp.jpg";
+// import RealCamp from "../assets/realCamp.JPG";
+
+const Main = () => {
+  return (
+    <div>
+      <img src={Camp} alt="" height="500px" />
+      <br />
+      {/* <img src={RealCamp} alt="" height="500px" /> */}
+    </div>
+  );
+};
+
+export default Main;
